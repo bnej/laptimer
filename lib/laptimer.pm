ba1f9cd9573352788f8efd8a564b7e87b21fac31
Ben Lilburne <benl@minime.villian>
2020-07-01 21:17:02 +1000

    Add stopwatch route.

--- lib/laptimer.pm
+++ lib/laptimer.pm
@@ -1,3 +1,6 @@
+use strict;
+use warnings;
+
 package laptimer;
 use Dancer ':syntax';
 
@@ -5,6 +8,11 @@ our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index';
+};
+
+get '/club/:club/stopwatch' => sub {
+    my $club = params->{club};
+    template 'stopwatch', { club => $club };
 };
 
 true;
