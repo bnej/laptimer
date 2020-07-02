16835877646a2868b3831b16ef7024d520328327
Ben Lilburne <bnej@mac.com>
2020-07-02 22:03:18 +1000

    Database Connection (local postgres) Get the club name from the DB Stopwatch start/stop Stopwatch doesn't show live MS Marks recorded in JSON for sync with server

--- lib/laptimer.pm
+++ lib/laptimer.pm
@@ -3,6 +3,7 @@ use warnings;
 
 package laptimer;
 use Dancer ':syntax';
+use Dancer::Plugin::Database;
 
 our $VERSION = '0.1';
 
@@ -12,7 +13,12 @@ get '/' => sub {
 
 get '/club/:club/stopwatch' => sub {
     my $club = params->{club};
-    template 'stopwatch', { club => $club };
+    my $sth = database->prepare(
+	"select * from club where clubid = ?"
+	);
+    $sth->execute($club);
+    my $cr = $sth->fetchrow_hashref();
+    template 'stopwatch', { club => $cr->{clubname} };
 };
 
 true;
--- public/javascripts/stopwatch.js
+++ public/javascripts/stopwatch.js
@@ -1,3 +1,59 @@
+var timer_start = Date.now();
+var num = 1;
+var timer_running = false;
+
+function start_stop( button ) {
+    if( timer_running ) {
+	stop_timer( button );
+    } else {
+	start_timer( button );
+    }
+}
+
+function start_timer( button ) {
+    timer_start = Date.now();
+    timer_running = true;
+
+    button.addClass('btn-danger');
+    button.removeClass('btn-success');
+    button.text("Stop");
+
+    update_time();
+}
+
+function stop_timer( button ) {
+    timer_running = false;
+
+    button.removeClass('btn-danger');
+    button.addClass('btn-success');
+    button.text("Start");
+}
+
+function update_time() {
+    var current = Date.now();
+    var delta = current - timer_start;
+    $('#timer-current').text( ms_format( delta,1 ) );
+    if( timer_running ) 
+	setTimeout(update_time, 100);
+}
+
+var timer_marks = [ ];
+
+function mark() {
+    var current = Date.now();
+    var delta = current - timer_start;
+    var delta_fmt = ms_format( delta );
+    timer_marks.push( {
+	time: delta,
+	time_fmt: delta_fmt,
+	sync: false,
+    });
+    $('#mark-table > tbody').prepend('<tr><th scope="row">'+num+'</th><td>'+delta_fmt+'</tr>');
+    num ++;
+}
+update_time();
+
+
 function zero_pad( n, width ) {
     var str = n.toString();
     width -= str.length;
@@ -8,12 +64,22 @@ function zero_pad( n, width ) {
     return str;
 }
 
-function ms_format( ms ) {
+function ms_format( ms, ms_places = 3 ) {
     var p_ms = ms % 1000; // milliseconds
     var p_seconds = Math.floor( ms / 1000 ); // Whole seconds
     var p_minutes = Math.floor( p_seconds / 60 ); // Whole minutes
 
+    if( ms_places == 2 )
+	p_ms = Math.floor( p_ms / 10 );
+    else if( ms_places == 1 )
+	p_ms = Math.floor( p_ms / 100 );
+
     p_seconds = p_seconds % 60; // Remove minutes
 
-    return "" + zero_pad( p_minutes, 2 ) + ":" + zero_pad( p_seconds, 2 ) + "." + zero_pad(p_ms, 3);
+    if( ms_places == 0 ) {
+	// Return without ms
+	return "" + zero_pad( p_minutes, 2 ) + ":" + zero_pad( p_seconds, 2 );
+    } else {
+	return "" + zero_pad( p_minutes, 2 ) + ":" + zero_pad( p_seconds, 2 ) + "." + zero_pad(p_ms, ms_places);
+    }
 }
