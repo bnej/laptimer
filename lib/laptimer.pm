5b4c85fe95f93006b2e3ddb833321cf1b7485a15
Ben Lilburne <bnej@mac.com>
2020-07-19 18:35:08 +1000

    Results screen.

--- lib/laptimer.pm
+++ lib/laptimer.pm
@@ -5,6 +5,7 @@ package laptimer;
 use Dancer ':syntax';
 use Dancer::Plugin::Database;
 use Dancer::Exception qw(:all);
+use POSIX qw(ceil floor);
 
 our $VERSION = '0.1';
 
@@ -129,6 +130,87 @@ get '/club/:club/:event/stopwatch' => sub {
 			    "baseurl" => "/club/$club/$event" };
 };
 
+get '/club/:club/:event/results' => sub {
+    my $club = params->{club};
+    my $event = params->{event};
+
+    my %athletes = ( );
+    my $sth_club = database->prepare(
+	"select * from club join event using (club_id) where club_id = ? and event_id = ?"
+	);
+    $sth_club->execute($club, $event);
+    my $cr = $sth_club->fetchrow_hashref();
+    my $final_lap_n = $cr->{start_lap} + $cr->{total_laps};
+    
+    my $sth = database->prepare(
+	"select * from place_mark join time_mark using (timing_number, event_id) where event_id = ? order by timing_number asc"
+	) or die database->errstr;
+    my $sth_a = database->prepare(
+	'select * from athlete where athlete_id = ?'
+	) or die database->errstr;
+    $sth->execute( $event );
+
+    while( my $r = $sth->fetchrow_hashref ) {
+	my $id = $r->{athlete_id};
+	my $time = $r->{timing_mark};
+	if( !$athletes{$id} ) {
+	    $athletes{$id} = {
+		laps => [ ],
+		fastest => undef,
+		total => 0,
+		event_laps => 0,
+		prior => 0,
+		name => undef,
+		finished => 0,
+	    };
+	}
+	next if $athletes{$id}{finished};
+
+	my $lap = $time - $athletes{$id}{prior};
+	$athletes{$id}{prior} = $time;
+	push @{$athletes{$id}{laps}}, $lap;
+	
+	my $event_laps = ++ $athletes{$id}{event_laps};
+	warning($event_laps);
+	if( $event_laps >= $cr->{total_laps} ) {
+	    warning("finished");
+	    $athletes{$id}{finished} = 1;
+	}
+	$athletes{$id}{total} += $lap;
+	if(defined $athletes{$id}{fastest}) {
+	    $athletes{$id}{fastest} = $lap if $lap < $athletes{$id}{fastest};
+	} else {
+	    $athletes{$id}{fastest} = $lap; # First lap in event
+	}
+    }
+    
+    foreach my $id (keys %athletes) {
+	$sth_a->execute( $id );
+	warning $id;
+	my $r = $sth_a->fetchrow_hashref;
+
+	$athletes{$id}{name} = $r->{athlete_name};
+    }
+    my @results_table = sort { $b->{finished} <=> $a->{finished} || $a->{total} <=> $b->{total} } values %athletes;
+    for( my $i = 0; $i < @results_table; $i ++ ) {
+	if( $results_table[$i]{finished} ) {
+	    $results_table[$i]{place} = $i + 1;
+	} else {
+	    $results_table[$i]{place} = "DNF";
+	}
+	$results_table[$i]{fastest} = ms_format($results_table[$i]{fastest});
+	$results_table[$i]{total} = ms_format($results_table[$i]{total});
+    }
+    
+    template 'results', {
+	"event_info" => $cr,
+	"baseurl" => "/club/$club/$event",
+        "cluburl" => "/club/$club",
+        "results" => \@results_table,
+    };
+
+};
+
 get '/club/:club/:event/lap' => sub {
     my $club = params->{club};
     my $event = params->{event};
@@ -144,5 +226,16 @@ get '/club/:club/:event/lap' => sub {
 	"cluburl" => "/club/$club",
     };
 };
+
+sub ms_format {
+    my $ms = shift;
+    my $ms_places = shift || 3;
+
+    my $p_ms = $ms % 1000; # milliseconds
+    my $p_seconds = floor( $ms / 1000 ); # Whole seconds
+    my $p_minutes = floor( $p_seconds / 60 ); # Whole minutes
+    
+    return sprintf('%1d:%02d.%0'.$ms_places.'d',$p_minutes,$p_seconds,$p_ms);
+}
 
 true;
